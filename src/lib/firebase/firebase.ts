410aadeeed98efe46acce1b864609721aec42f00
callmedeci <callmedecim@gmail.com>
2025-06-25 09:28:04 -0700

    [Fix] applyActionCodeForVerification function arguments

--- src/lib/firebase/firebase.ts
+++ src/lib/firebase/firebase.ts
@@ -1,8 +1,7 @@
-import { initializeApp, getApps, getApp, type FirebaseOptions } from 'firebase/app';
-import { getAuth, applyActionCode } from 'firebase/auth';
+import { getApp, getApps, initializeApp } from 'firebase/app';
+import { applyActionCode, getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
-import { getMessaging } from 'firebase/messaging';
 import { firebaseConfig } from '../constants';
 
 // Initialize Firebase
@@ -10,4 +9,5 @@ export const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
-export const applyActionCodeForVerification = applyActionCode;
+export const applyActionCodeForVerification = (oobCode: string) =>
+  applyActionCode(auth, oobCode);
