0bab00b60a727f2d37c6ff9608922a1b559d2dfa
Parsa Sobhani <parsamax24@gmail.com>
2025-07-03 20:53:44 +0000

    // For Firebase JS SDK v7.20.0 and later, measurementId is optional cons

--- src/app/api/user/serverApp.ts
+++ src/app/api/user/serverApp.ts
@@ -4,8 +4,16 @@
 import { FirebaseApp, FirebaseError, initializeApp, initializeServerApp,FirebaseServerApp } from "firebase/app";
 import { cookies } from "next/headers";
 import { getAuth, NextOrObserver, Unsubscribe, User } from "firebase/auth";
-import { firebaseConfig } from "../../../lib/constants";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBJQkR3v-DtLo7bRZ3nhENneMeT54OLkKA",
+  authDomain: "project2-52c08.firebaseapp.com",
+  projectId: "project2-52c08",
+  storageBucket: "project2-52c08.firebasestorage.app",
+  messagingSenderId: "132408690151",
+  appId: "1:132408690151:web:bb74c7c4a353a07bec6942",
+  measurementId: "G-89TMTTT0B7"
+};
 
 export interface IAuthincatedAppUser{
     firebaseServerApp:FirebaseApp,
--- src/lib/firebase/firebase.ts
+++ src/lib/firebase/firebase.ts
@@ -4,12 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBJQkR3v-DtLo7bRZ3nhENneMeT54OLkKA",
+  authDomain: "project2-52c08.firebaseapp.com",
+  projectId: "project2-52c08",
+  storageBucket: "project2-52c08.firebasestorage.app",
+  messagingSenderId: "132408690151",
+  appId: "1:132408690151:web:bb74c7c4a353a07bec6942",
+  measurementId: "G-89TMTTT0B7"
 };
 
 // Initialize Firebase
