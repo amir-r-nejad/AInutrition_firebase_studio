2a5ad9e7f6217cb1100385b47bae2401152b8e8c
parsamax24 <parsamax24@gmail.com>
2025-07-14 22:04:03 +0000

    Adding some property to globaltypes.ts

--- src/types/globalTypes.ts
+++ src/types/globalTypes.ts
@@ -2,4 +2,6 @@ export type User = {
   uid: string;
   email: string | null;
   emailVerified: boolean;
+  displayName: string | null; // Add displayName property
+  photoURL: string | null; // Add photoURL property
 };
